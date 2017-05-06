4d42e050b9c3c7a82dc2ec25ab43f018530f12a2
unknown <zhigang.zhang@QF-zhangzgang.qunarservers.com>
2017-05-06 13:13:25 +0800

    Signed-off-by: unknown <zhigang.zhang@QF-zhangzgang.qunarservers.com>

---
+++ kata-es6/log/log.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zhigang.zhang on 17-5-6.
+ */
+{
+    const STYLES={
+        get title(){
+            return 'color:blue;font-size:14px;font-weight:bolder;background-color:yellow;'
+        },
+        get subtitle(){
+            return 'color:red;font-size:12px;background-color:yellow;'
+        }
+    }
+    Object.assign(window,{
+        title:function(title){
+            console.log(`%c ${title}`,`${STYLES.title}`);
+        },
+        log:function(log){
+            console.log(log);
+        },
+        'subtitle':function(title){
+            console.log(`%c ${title}`,`${STYLES.subtitle}`);
+        }
+    });
+}
