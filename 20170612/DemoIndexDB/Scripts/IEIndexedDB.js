071e8d11eb101f98644eac8857aa7bf4c35db9fb
unknown <zhigang.zhang@QF-zhangzgang.qunarservers.com>
2017-06-12 19:55:48 +0800

    Signed-off-by: unknown <zhigang.zhang@QF-zhangzgang.qunarservers.com>

---
+++ 20170612/DemoIndexDB/Scripts/IEIndexedDB.js
@@ -0,0 +1,32 @@
+﻿var idbSupported = false;
+var db;
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    if ("indexedDB" in window) {
+        idbSupported = true;
+    }
+
+    if (idbSupported) {
+        var openRequest = indexedDB.open("employees", 1);
+
+        openRequest.onupgradeneeded = function (e) {
+            writeToConsoleScreen('Upgrading');
+            console.log("Upgrading...");
+        }
+
+        openRequest.onsuccess = function (e) {
+            writeToConsoleScreen('Success');
+            console.log("Success!");
+            db = e.target.result;
+        }
+
+        openRequest.onerror = function (e) {
+            writeToConsoleScreen('Error');
+            console.log("Error");
+            console.dir(e);
+        }
+
+    }
+
+}, false);
