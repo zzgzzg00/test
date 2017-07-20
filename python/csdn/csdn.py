354c02b6c83fddaff6259cdd1cae086be753db1c
zhigang.zhang <zzgzzg00@163.com>
2017-07-20 09:31:49 +0800

    Signed-off-by: zhigang.zhang <zzgzzg00@163.com>

--- python/csdn/csdn.py
+++ python/csdn/csdn.py
@@ -1,27 +1,4 @@
-import csv,pickle
-def formatToDict(li):
-    return {
-        'name':li[0],
-        li[1]:li[2],
-        li[3]:li[4]
-    }
-def read():
-    li=[]
-    f=open('d://test.csv','r')
-    t=csv.reader(f)
-    for row in t:
-        li.append(formatToDict(row))
-    f.close()
-    return li
-
-def write():
-    with open('d://test.txt','wb') as f:
-        pickle.dump(read(),f)
-
-def load():
-    with open('d://test.txt','rb') as f:
-        result=pickle.load(f)
-    return result
-
-write()
-print(load())
+import re;
+str='a;bc;d;'
+reg=re.compile(r';')
+print(reg.split(str))
