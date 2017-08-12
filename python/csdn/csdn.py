1d5702dd915a22edd7abbd01c1c938497e2d3691
zhigang.zhang <zzgzzg00@163.com>
2017-08-12 20:17:12 +0800

    Signed-off-by: zhigang.zhang <zzgzzg00@163.com>

--- python/csdn/csdn.py
+++ python/csdn/csdn.py
@@ -1,3 +1,5 @@
-import random
-p=random.choice([-1,1])*random.random()
-print(p)
+import re
+lists=['name','age']
+reg=re.compile(r'(?P<{0}>[a-z]+)(?P<{1}>\d+)'.format(*lists))
+result=reg.match('abc123')
+print([result.group(i) for i in lists])
