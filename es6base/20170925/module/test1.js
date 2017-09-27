406915cd4c7b9bc7664f936bfa8e8f127cadb497
zhigang.zhang <zzgzzg00@163.com>
2017-09-27 10:01:38 +0800

    Signed-off-by: zhigang.zhang <zzgzzg00@163.com>

---
+++ es6base/20170925/module/test1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zhigang.zhang on 17-9-27.
+ */
+let a=1;
+export default a;//1
+//export {a as default}; //2
+a=2;
---
+++ es6base/20170925/module/test2.js
@@ -0,0 +1,5 @@
+/**
+ * Created by zhigang.zhang on 17-9-27.
+ */
+import a from './test1.js'
+console.log(a);
