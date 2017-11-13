f4a8a2be43755bc21208e00b8d55ddb3f6771947
zhigang.zhang <zzgzzg00@163.com>
2017-11-13 19:09:56 +0800

    Signed-off-by: zhigang.zhang <zzgzzg00@163.com>

---
+++ es6base/20171113/e.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zhigang.zhang on 17-11-13.
+ */
+function t1(){
+    console.log('t1');
+}
+function t2(){
+    console.log('t2');
+}
+export {t1 as default ,t2};
---
+++ es6base/20171113/i.js
@@ -0,0 +1,6 @@
+/**
+ * Created by zhigang.zhang on 17-11-13.
+ */
+import a,{t2} from './e.js';
+a();
+t2();
