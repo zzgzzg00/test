a2f7d726c006e7fc34f6352a5240cedf35c45a9a
zhigang.zhang <zhigang.zhang@qunar.com>
2018-09-19 10:26:19 +0800

    add

--- FE/2018/import/js/main.js
+++ FE/2018/import/js/main.js
@@ -1,5 +1,7 @@
 import sub from './sub1.js';
 sub();
+// alert(a);
+// setTimeout(alert,2000,a)
 document.body.onclick=function () {
     import('./test.js');
 }
---
+++ FE/2018/import/js/test2.js
@@ -0,0 +1,3 @@
+let a=1;
+setTimeout(()=>a=2,1000);
+export default a;
