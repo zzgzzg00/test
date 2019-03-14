bba1e602b2b853d1649beed21e9fc74f515c9fdf
zhigang.zhang <zhigang.zhang@qunar.com>
2019-03-14 23:44:01 +0800

    add

---
+++ FE/temp.js
@@ -0,0 +1,33 @@
+/**
+ * [
+ {
+     "gender": 1,
+     "ticketPrice": "¥5.5",
+     "ticketStatus": 1,
+     "ticketStatusDesc": "",
+     "lastPayTime": "",
+     "seatNo": "02车厢01A号",
+     "ticketType": 0,
+     "isTicketChange": false,
+     "refundAmount": "0",
+     "ticketNo": "EA96193897",
+     "certNoObj": {
+         "display": "1402***********510",
+         "value": "140211198505110510",
+         "prenum": ""
+     },
+     "subOrderNo": "",
+     "ticketChange": false,
+     "refundFee": "0",
+     "ticketTypeStr": "成人票",
+     "certType": "身份证",
+     "seat": "二等座",
+     "certNo": "1402z4ygVAHX==G510",
+     "insureCount": 0,
+     "name": "张志刚",
+     "packageCount": 0,
+     "passengerName": "张志刚",
+     "birthday": "1985-05-11"
+ }
+ ]
+ * */
